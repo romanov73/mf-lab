faadfaf6612206f2b14a74093e13dcd5182473c3
Amon <sergpolevoi@yandex.ru>
2024-02-23 01:33:04 +0400

    Бета версия вёрстки главной страницы по макету

--- base/urls.py
+++ base/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import web_page.views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', web_page.views.index, name='main')
 ]
