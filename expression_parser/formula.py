254b7f116f957c873cf179baf0c24c2f28018b64
M Kuzarin <kuzarinm@mail.ru>
2024-02-22 23:12:07 +0400

    Исправления недоработок, выявленных во время просмотра PR

--- expression_parser/formula.py
+++ expression_parser/formula.py
@@ -8,10 +8,8 @@ class Formula:
         """
         Формула, которую можно вычислить. Для этого необходимо передать значения всех переменных.
 
-        Поля класса:
+        Поля экземпляра:
             expression (str): текстовое представление формулы после предобработки (в случае ошибки валидации - None)
-
-            variables (dict[float]): словарь, где ключ - переменная, а значение - её значение(изначально - None)
 
             error_text (str): текст ошибки, если таковая имеется
 
@@ -22,15 +20,17 @@ class Formula:
         """
         expression = self._preprocess_formula(expression)
         error_position: int = self._formula_validation(expression)
+        self.error_text = None
+        self.error_position = None
 
         if error_position is None:
             self.expression: str = expression
-            self.variables: dict[float] = self._extract_all_variable()
-            if self.variables is None:
+            self._variables: dict[str, float] = self._extract_all_variable()
+            if self._variables is None:
                 self.expression = None
         else:
             self.expression = None
-            self.variables = {}
+            self._variables = {}
             self.error_text = "Ошибка валидации формулы"
             self.error_position = error_position
 
@@ -66,7 +66,7 @@ class Formula:
         """
         return formula_str.replace("^", "**")
 
-    def _extract_all_variable(self) -> dict[float] | None:
+    def _extract_all_variable(self) -> dict[str, float] | None:
         """
         Получение списка всех переменных, значение которых необходимо задать, для вычисления значения по формуле.
         В случае ошибок, они будут записаны в поле error_text
@@ -77,7 +77,7 @@ class Formula:
         """
         try:
             return {i[0]: None for i in regex.findall(
-                r'(\b[a-z]\w*\b(?!\s*[\(\"])(\[(?:[^\[\]]|(?2))*\])?)',
+                r'(\b[a-zA-Z]\w*\b(?!\s*[\(\"])(\[(?:[^\[\]]|(?2))*\])?)',
                 self.expression,
                 overlapped=True)}
         except Exception as ex:
@@ -93,19 +93,31 @@ class Formula:
         Returns:
             float: Результат вычисления.
         """
-        if self.expression is None or self.variables is None:
+        if self.expression is None or self._variables is None:
             return None
 
-        if any([i is None for i in self.variables.values()]):
+        if any([i is None for i in self._variables.values()]):
             self.error_text = "Не все переменные заполнены, что недопустимо"
             return None
 
         try:
             self.error_text = None
-            return ne.evaluate(self.expression, local_dict=self.variables).item()
+            return ne.evaluate(self.expression, local_dict=self._variables).item()
         except ArithmeticError as ae:
             self.error_text = f"Арифметическая ошибка:{str(ae)}"
         except Exception as ex:
             self.error_text = str(ex)
 
         return None
+
+    def set_variables(self, variables: dict[str, float]):
+        """
+        Метод создан для передачи информации о переменных в формате словаря.
+        Имеется проверка на наличие потребности в передаваемой переменной, чтобы зря не грузить память
+
+        Args:
+            variables (dict[str, float]): словарь передаваемых переменных и значений
+        """
+        for i in variables.keys():
+            if i in self._variables.keys():
+                self._variables[i] = variables[i]
