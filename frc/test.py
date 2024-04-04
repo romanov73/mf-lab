cbf2656d426839809518c27584bb2fc3b865332a
shadowik <zahar.mark@mail.ru>
2024-04-04 18:15:12 +0400

    Генерация документа с множеством формул

--- frc/test.py
+++ frc/test.py
@@ -8,22 +8,44 @@ if __name__ == "__main__":
     file = DocxReport()
 
     context = {
-        "variables": [
+        "data": [
             {
-                "name": "Test1",
-                "value": 1.23
+            "variables": [
+                {
+                    "name": "Test1",
+                    "value": 1.23
+                },
+                {
+                    "name": "Test2",
+                    "value": 1.2321
+                },
+                {
+                    "name": "Test3",
+                    "value": 1
+                },
+            ],
+            "formula": "Test1 + Test2 + Test3",
+            "result": 1.23 + 1.2321 + 1
             },
             {
-                "name": "Test2",
-                "value": 1.2321
-            },
-            {
-                "name": "Test3",
-                "value": 1
-            },
-        ],
-        "formula": "Test1 + Test2 + Test3",
-        "result": 1.23 + 1.2321 + 1
+                "variables": [
+                    {
+                        "name": "Test1",
+                        "value": 1.23
+                    },
+                    {
+                        "name": "Test2",
+                        "value": 1.2321
+                    },
+                    {
+                        "name": "Test3",
+                        "value": 1
+                    },
+                ],
+                "formula": "Test1 + Test2 + Test3",
+                "result": 1.23 + 1.2321 + 1
+            }
+        ]
     }
     file.render(context)
 
