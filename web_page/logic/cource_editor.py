e415f200d7f202df626ebce41608a450af36ec0f
M Kuzarin <kuzarinm@mail.ru>
2024-04-18 22:24:50 +0400

    Добавлен UI для привязки групп к курсам. Так же небольшой фикс в области фалов

--- web_page/logic/cource_editor.py
+++ web_page/logic/cource_editor.py
@@ -8,7 +8,7 @@ from django.core.files.storage import default_storage
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 
-from web_page.models import Course, File
+from web_page.models import Course, File, Group
 from web_page.utils import for_teacher
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -35,7 +35,8 @@ def create_course_view(request):
                   context={
                       "creating_title": "Создание курса",
                       "button_text": "Создать курс",
-                      "creating_url": reverse('create_course')
+                      "creating_url": reverse('create_course'),
+                      "groups": list(Group.objects.all().values("id", "name"))
                   })
 
 
@@ -50,17 +51,28 @@ def edit_course_view(request, id: int):
                       "object_name": course.name,
                       "object_summary": course.summary,
                       "object_description": course.description,
-                      "files": [i.id for i in File.objects.filter(course=course)]
+                      "files": [i.id for i in File.objects.filter(course=course)],
+                      "groups": list(Group.objects.all().values("id", "name")),
+                      "my_groups": list(i["id"] for i in course.groups.values("id"))
                   })
 
 
 def add_course_action(request):
+    groups = []
+    try:
+        groups = list(map(int, request.POST.get("groups").split("|")))
+    except:
+        pass#Что-то тут надо другое
+
     course: Course = Course(
         name=request.POST.get("name"),
         summary=request.POST.get("summary"),
-        description=request.POST.get("text")
+        description=request.POST.get("text"),
+        user=request.user
     )
     course.save()
+    course.groups.set(Group.objects.filter(id__in=groups).all())
+
 
     if request.POST["attachments[]"]:
         for fid in request.POST["attachments[]"].split("|"):
@@ -74,10 +86,17 @@ def add_course_action(request):
 
 
 def update_course_action(request, id):
+    groups = []
+    try:
+        groups = list(map(int, request.POST.get("groups").split("|")))
+    except:
+        pass#Что-то тут надо другое
+
     course: Course = get_object_or_404(Course, id=id)
     course.name = request.POST.get("name")
     course.summary = request.POST.get("summary")
     course.description = request.POST.get("text")
+    course.groups.set(Group.objects.filter(id__in=groups).all())
     course.save()
 
     if request.POST["attachments[]"]:
