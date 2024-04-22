946bad0804d149ca1b5f3834a5d2b726a3c6f89f
shadowik <zahar.mark@mail.ru>
2024-04-22 08:52:01 +0400

    Настройка статики и изменение пути к env

--- base/settings.py
+++ base/settings.py
@@ -23,7 +23,7 @@ env = environ.Env(
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+environ.Env.read_env(os.path.join(BASE_DIR.parent, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
@@ -140,6 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT= os.path.join(BASE_DIR, 'static_media/')
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
