961cdb898ad0b661ceb247227533367092faa423
shadowik <zahar.mark@bk.ru>
2024-10-16 09:11:26 +0400

    Обновления для сервера

--- load_users/main.py
+++ load_users/main.py
@@ -1,8 +1,10 @@
 import os
+import sys
 import django
 
 
 if __name__ == "__main__":
+    sys.path.append("..")
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'base.settings')
     django.setup()
 
